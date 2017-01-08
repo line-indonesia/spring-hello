e14bb629d5749a9844f547abb9bf41ea906ccc54
Matthew Tanudjaja <mrexmelle@gmail.com>
2017-01-08 14:49:50 +0700

    refactor: Use GetMapping instead of RequestMapping

--- src/main/java/com/linecorp/example/springhello/HelloController.java
+++ src/main/java/com/linecorp/example/springhello/HelloController.java
@@ -4,16 +4,13 @@ package com.linecorp.example.springhello;
 import java.time.LocalDateTime;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value="/")
 public class HelloController
 {
-    @RequestMapping(value="/hello", method=RequestMethod.GET)
+    @GetMapping(value="/hello")
     public ResponseEntity<String> hello(
         @RequestParam(value="name", required=false) String aName)
     {
